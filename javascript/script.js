09a4a53cc3058ba6b6248e80ed7aee1358e24876
KoSeongMin1 <50435120+KoSeongMin1@users.noreply.github.com>
2019-12-23 11:24:34 +0900

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+$(".nav>li").mouseover(function(){
+    $(this).children(".submenu").stop().slideDown();
+});
+$(".nav>li").mouseleave(function(){
+    $(this).children(".submenu").stop().slideUp();
+});
+
+var slideSpeed=2800;
+
+$(".slide").children("a:gt(0)").hide();
+
+var end=setInterval(function(){
+    $(".slide").find("a:first").stop().fadeOut(slideSpeed)
+    .next()
+    .fadeIn(slideSpeed)
+    .end()
+    .appendTo(".slide")
+},slideSpeed);
+
+
+$("#noticeBtn").click(function(){
+    $("#notice").show();
+    $("#noticeBtn").css({color:"#333333"});
+    $("#gallery").hide();
+    $("#galleryBtn").css({color:"lightgray"});
+});
+$("#galleryBtn").click(function(){
+    $("#gallery").show();
+    $("#galleryBtn").css({color:"#333333"});
+    $("#notice").hide();
+    $("#noticeBtn").css({color:"lightgray"});
+});
+
+$("#noticeBtn").focus(function(){
+    $("#notice").show();
+    $("#noticeBtn").css({color:"#333333"});
+    $("#gallery").hide();
+    $("#galleryBtn").css({color:"lightgray"});
+});
+$("#galleryBtn").focus(function(){
+    $("#gallery").show();
+    $("#galleryBtn").css({color:"#333333"});
+    $("#notice").hide();
+    $("#noticeBtn").css({color:"lightgray"});
+});
+
+$(".onClick").click(function(){
+    $("#modal").show();
+})
+$("#modalcontent>button").click(function(){
+    $("#modal").hide();
+})
